40c7d0a6afef91c99d5310c10eed2b886f34ffcf
monktour2384 <149628279+monktour2384@users.noreply.github.com>
2024-10-29 06:45:54 +0100

    first commit

---
+++ src/components/Textarea.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Proptypes from "prop-types";
+
+export default function Textarea(props) {
+  const handleupclick = () => {
+    const newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleloclick = () => {
+    const newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const handleClear = () => {
+    const newText = "";
+    setText(newText);
+  };
+
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div className="container">
+        <div className="mb-3">
+          <h1>{props.h1}</h1>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="16"
+            onChange={handleOnChange}
+            value={text}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleupclick}>
+          To upperClass
+        </button>
+        <button className="btn btn-primary" onClick={handleloclick}>
+          To lowerClass
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleClear}>
+          Clear text
+        </button>
+        <div className="container">
+          <p>
+            {text.split(" ").length} Words {text.length} characters
+          </p>
+          <p>Minutes to read {0.008 * text.split(" ").length}</p>
+          <p>Peview</p>
+          <p>{text}</p>
+        </div>
+      </div>
+    </>
+  );
+}
